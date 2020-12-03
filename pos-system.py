c84e07c60fbd07a083eff4af4ea48c037bf7351e
hiroakiitou <pigaru.gooood.mar19@gmail.com>
2020-12-03 23:35:05 +0900

    コード整理

--- pos-system.py
+++ pos-system.py
@@ -1,3 +1,6 @@
+import pandas as pd
+import datetime
+
 ### 商品クラス
 class Item:
     def __init__(self,item_code,item_name,price):
@@ -18,26 +21,77 @@ class Order:
         self.item_order_list.append(item_code)
         
     def view_item_list(self):
-        for item in self.item_order_list:
-            print("商品コード:{}".format(item))
+        search_result = False
+        for item in self.item_master:
+            for order_item in self.item_order_list:
+                if int(order_item) == item.item_code:
+                    print("オーダーは\n商品コード:{}、商品名:{}、価格:{}円ですね。".format(item.item_code, item.item_name, item.price))
+                    search_result = True
+                    calc(item)
+        if search_result:
+            return True
+        return False
+
+def calc(item):
+    mount = input("数量を入力してください：")
+    mount = num_check(mount, "数量")
+
+    total_price = item.price * int(mount)      
+    print("【注文確認】\n商品コード:{}、商品名:{}、個数{}、合計金額:{}円ですね。".format(item.item_code, item.item_name, mount, total_price))
     
-    
+    pay = input("お支払い金額を入力してください:")
+    pay = pay_check(pay, total_price)
+
+    change = int(pay) - total_price
+    print("お釣りは{}円です。".format(change))
+    # レシートファイル出力
+    now = datetime.datetime.now().strftime('%Y%m%d_%H%M%S')
+    receipt_file_name = "{}.txt".format(now)
+    with open(receipt_file_name,'w', encoding='utf_8-sig') as f:
+        f.write("商品コード:{}\n商品名:{}\n数量:{}個\n合計金額:{}円\nお支払い金額:{}円\nお釣り:{}円".format(item.item_code, item.item_name, mount, total_price, pay, change))
+
+    print("ありがとうございました。")
+
+# 入力値が文字列なので、数値変換が可能な値なら変換した値を返す
+def num_check(num_object, message_item):
+    check = False
+    while(check == False):
+        if not num_object or not num_object.isdecimal():
+            num_object = input("不正な値です{}は半角数字で入力してください：".format(message_item))
+        else:
+            check = True
+    return num_object
+
+def pay_check(pay_object, total_price):
+    pay_object = num_check(pay_object, "金額")
+    check = False
+    while(check == False):
+        if (int(pay_object) - total_price) < 0:
+            pay_object = input("金額が足りません。もう一度入力してください：")
+        else:
+            check = True
+    return pay_object
+      
 ### メイン処理
 def main():
     # マスタ登録
     item_master=[]
-    item_master.append(Item("001","りんご",100))
-    item_master.append(Item("002","なし",120))
-    item_master.append(Item("003","みかん",150))
+    source = pd.read_csv("item.csv").values.tolist()
+    for row in source:
+        item_master.append(Item(row[0], row[1], row[2]))
     
     # オーダー登録
     order=Order(item_master)
-    order.add_item_order("001")
-    order.add_item_order("002")
-    order.add_item_order("003")
-    
+
     # オーダー表示
-    order.view_item_list()
+    order_result = False
+    while(order_result == False):
+        custommer_order = input("ご注文を商品番号でどうぞ：")
+        order.add_item_order(custommer_order)
+        if order.view_item_list():
+            order_result = True
+        else:
+            print("ご注文の商品はありません。もう一度やり直してください")
     
 if __name__ == "__main__":
     main()
